3a77faaea09c48779ab8aa203399d2ddff662b47
MD MONSUR ALAM <monsuralam0565@gmail.com>
2026-08-01 20:11:24 +0600

    chore: add harmless source comment

--- src/main.c
+++ src/main.c
@@ -118,3 +118,5 @@ int main(int argc, char *argv[]) {
     /* Return 0 = success, 1 = errors */
     return (parse_ok != 0 || semantic_error_count > 0) ? 1 : 0;
 }
+
+/* Non-functional note: this comment does not alter program behavior. */
